d717c3afb744f6370ca7e5c6172e7d0716309aa3
M-Xylene <manishpandey0.connect@gmail.com>
2025-06-03 20:19:55 +0530

    fix(Footer): stickiness.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="flex sticky bottom-3 justify-center items-center opacity-45">
+    <div className="flex sticky bottom-4 justify-center items-center opacity-45">
       &copy; {new Date().getFullYear()} Manish Pandey. All Rights Reserved.
     </div>
   )
