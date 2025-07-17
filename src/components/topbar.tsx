2ce875750d0ebc9cd069d216f64d2d88187f4148
M-Xylene <manishpandey0.connect@gmail.com>
2025-07-17 16:46:52 +0530

    fix(Navbar): Z-index ignored during build.

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -7,7 +7,7 @@ export default function Topbar() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ ease: "easeOut", duration: 1 }}
-      className="navbar flex z-1 sticky top-4 items-center p-2 md:p-0 text-gray-500 rounded-[0.8rem] outline outline-[#ffffff70]" >
+      className="navbar flex z-10 sticky top-4 items-center p-2 md:p-0 text-gray-500 rounded-[0.8rem] outline outline-[#ffffff70]" >
 
       {/* Left part */}
       <div className="text-[#ffffff] shrink text-[1.1rem] font-bold md:text-[1.5rem] md:ml-7 ml-2 justify-start">
