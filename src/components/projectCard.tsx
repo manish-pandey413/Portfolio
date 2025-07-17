312974c4976fcb596160c4837442215988acfaaf
M-Xylene <manishpandey0.connect@gmail.com>
2025-07-17 16:31:20 +0530

    ProjectCard component.

---
+++ src/components/projectCard.tsx
@@ -0,0 +1,26 @@
+import { motion } from "motion/react";
+
+const itemVariant = {
+  hidden: {
+    opacity: 0,
+    x: -40
+  },
+  show: {
+    opacity: 1,
+    x: 0,
+    ease: "easeOut"
+  }
+}
+
+export default function ProjectCard() {
+  return (
+    <motion.div
+      className="w-[30rem] h-[25rem] bg-[#1d2021] p-4 rounded-lg m-4"
+      variants={itemVariant}
+    >
+      <motion.text className="justify-center my-[4rem] content-center text-[2rem] md:text-[3rem] font-[Poppins] font-bold opacity-40">
+        Nothing Yet :(
+      </motion.text>
+    </motion.div>
+  )
+}
