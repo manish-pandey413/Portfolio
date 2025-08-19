29dc6e2003eaf58b2def993b428709aa58082d18
M-Xylene <manishpandey0.connect@gmail.com>
2025-08-20 01:59:21 +0530

    feat(UI): Project Card component and project.

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,26 +1,58 @@
 import { motion } from "framer-motion";
+import type { ReactNode } from "react";
+import { TfiGithub as GitIcon } from "react-icons/tfi";
+
+interface Props {
+  ProjectName: string;
+  ProjectLink: string;
+  ImagePath: string;
+  children: ReactNode;
+}
 
 const itemVariant = {
   hidden: {
     opacity: 0,
-    x: -40
+    scale: 0
   },
   show: {
     opacity: 1,
-    x: 0,
+    scale: 1,
     ease: "easeOut"
   }
 }
 
-export default function ProjectCard() {
+export default function ProjectCard({ ProjectName, ProjectLink, ImagePath, children }: Props) {
   return (
     <motion.div
-      className="w-[25rem] h-[25rem] bg-[#1d2021] p-4 rounded-lg m-4"
+      className="w-[20rem] overflow-hidden md:w-[25rem] bg-[#131313] rounded-lg m-4 md:m-8"
       variants={itemVariant}
     >
-      <motion.text className="justify-center my-[4rem] content-center text-[2rem] md:text-[3rem] font-[Poppins] font-bold opacity-40">
-        Nothing Yet :(
-      </motion.text>
+      <div className='flex justify-center align-center'>
+        <img className='w-[20rem] h-[13rem] md:w-[25rem] bg-[#1d2021] h-[18rem]' src={ImagePath} alt={ProjectName} />
+      </div>
+
+      <div className="p-4">
+        <motion.text className="text-[2rem] md:text-[3rem] ml-[0.9rem] font-[Poppins] font-bold opacity-70">
+          {ProjectName}
+        </motion.text>
+        <motion.text className="text-[1rem] m-4 md:text-[1.1rem] opacity-50">
+          {children}
+        </motion.text>
+
+        <motion.button
+          className="hover:cursor-pointer bg-green-500/7 text-green-500/70 px-[0.6rem] mr-[1rem] mt-4 rounded-[9rem]"
+          onClick={() => {
+            const link = document.createElement("a");
+            link.href = ProjectLink;
+            link.target = "_blank";
+            link.rel = "noopener noreferrer";
+            link.click();
+          }}
+        >
+          <GitIcon className="inline-flex align-center mb-[0.2rem] mr-[0.3rem]" /><text className="mb-[0.1rem]">Github</text>
+        </motion.button>
+
+      </div>
     </motion.div>
   )
 }
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import ProjectCard from "../components/projectCard";
 
 const projectContainer = {
   hidden: { opacity: 0 },
@@ -16,12 +17,19 @@ const projectContainer = {
 export default function Projects() {
   return (
     <motion.div
+      className="flex flex-wrap flex-col md:flex-row mt-[7rem] justify-center md:justify-evenly items-center"
       variants={projectContainer}
       initial="hidden"
       animate="show">
-      <p className="justify-center content-center text-[2rem] h-[92vh] md:text-[3rem] font-[Poppins] font-bold opacity-40">
-        Coming Soon :)
-      </p>
+
+      <ProjectCard
+        ProjectName="UNotes"
+        ProjectLink="https://github.com/manish-pandey413/ReactNative-App"
+        ImagePath=""
+      >
+        <p>A full stack cross platform mobile application to keep track and manage your expenses. Made using <text className="font-semibold px-1">React Native</text> and <text className="font-semibold px-1">Expo</text>.</p>
+      </ProjectCard>
+
     </motion.div>
   )
 }
